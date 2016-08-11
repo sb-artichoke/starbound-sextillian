4881841ce258f33a8f35af5974c575bd9e00708c
Arty <starbound.artichoke@gmail.com>
2016-08-10 22:32:16 -0400

    adding build script, small updates

---
+++ build.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+./asset_packer ./Sextillian-Items ./dist/Sextillian-Items.pak
+./asset_packer ./Sextillian-NPCs ./dist/Sextillian-NPCs.pak
+./asset_packer ./Sextillian-Objects ./dist/Sextillian-Objects.pak
+./asset_packer ./Sextillian-Species ./dist/Sextillian-Species.pak
